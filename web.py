cb827b84619028f731afccf021cca3549b3d2b48
romadv <advi0001@flinders.edu.au>
2023-03-12 18:01:18 +1100

    initial commit

---
+++ web.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import functions
+
+
+def add_todo():
+    todos = functions.get_todos()
+    todo = st.session_state["new_todo"]
+    todos.append(todo + "\n")
+    functions.write_todos(todos)
+
+
+todos = functions.get_todos()
+
+st.title("My Todo App")
+st.subheader("This is my todo app.")
+st.write("This app is to increase your productivity.")
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st._rerun()
+
+
+st.text_input(label="Add a new todo...", label_visibility="hidden", placeholder="Add a new todo...",
+              on_change=add_todo, key="new_todo")
+
